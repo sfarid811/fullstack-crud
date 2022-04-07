508daa455a4e6a81663267b593b481f02fe70c76
Saad <saadfarid029@gmail.com>
2022-04-07 23:26:47 +0200

    ENV BUILD

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const morgan = require('morgan');
 require('dotenv').config();
 const connectDB = require('./config/database');
 const todoRoutes = require('./routes/todos');
@@ -19,7 +19,15 @@ app.get('/', function(req, res) {
 
 app.use('/api/todos', todoRoutes);
 
-  
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
