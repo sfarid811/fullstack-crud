b0643dae636578a24fb4b550cd1293b6ea83b582
Saad <saadfarid029@gmail.com>
2022-04-08 15:34:22 +0200

    cleanup APP

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./features/todoSlice";
-import darkReducer from "./features/todoSlice";
+
 
 export const store = configureStore({
   reducer: {
     todo: todoReducer,
-    // dark: darkReducer
   },
 });
